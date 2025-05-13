43225ca39d0aed53c098e9c5e6b518bee87daabd
Denzel Ysai Bautista Rodriguez <129910983+denzelbautista@users.noreply.github.com>
2025-05-13 14:40:58 -0500

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_app(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Laboral3870859@database-laboral-empresas-1.cblrwbmmzkzn.us-west-2.rds.amazonaws.com:5432/laboraldb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Laboral111222#@database-laboral.chb5yvh5leot.us-east-2.rds.amazonaws.com:5432/laboraldb'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
